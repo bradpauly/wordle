01b6f87e8b85daefc48ed765065bd33dd6a64459
Brad Pauly <brad@convertkit.com>
2022-01-13 08:38:18 -0500

    Tests for hit, match, and miss.

--- spec/wordle_spec.rb
+++ spec/wordle_spec.rb
@@ -3,16 +3,34 @@ require "./lib/wordle"
 
 describe Wordle do
   describe "#guess" do
-    it "returns true when the given word matches" do
-      wordle = Wordle.new("testing")
+    it "returns an array of hits" do
+      wordle = Wordle.new("tests")
 
-      assert wordle.guess("testing")
+      assert_equal [2, 2, 2, 2, 2], wordle.guess("tests")
     end
 
     it "is case insensitive" do
-      wordle = Wordle.new("testing")
+      wordle = Wordle.new("tests")
 
-      assert wordle.guess("TESting")
+      assert_equal [2, 2, 2, 2, 2], wordle.guess("tests")
     end
+  end
+
+  it "returns an array of misses" do
+    wordle = Wordle.new("tests")
+
+    assert_equal [0, 0, 0, 0, 0], wordle.guess("abcda")
+  end
+
+  it "returns an array with matches" do
+    wordle = Wordle.new("tests")
+
+    assert_equal [0, 1, 0, 0, 0], wordle.guess("atcda")
+  end
+
+  it "returns an array with misses, matches, and hits" do
+    wordle = Wordle.new("tests")
+
+    assert_equal [2, 1, 0, 0, 0], wordle.guess("tsacd")
   end
 end
