87b9a0adc7edaecd31c342148d4a9dea8e46ab95
Brad Pauly <brad@convertkit.com>
2022-01-13 19:04:53 -0500

    Return hits, matches, and misses.

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -1,9 +1,16 @@
 class Wordle
   def initialize(answer)
-    @answer = answer.to_s.upcase
+    @answer = answer.to_s.upcase.split(//)
   end
 
   def guess(word)
-    word.to_s.upcase == @answer
+    letters = word.to_s.upcase.split(//)
+    letters.map do |letter|
+      if @answer.include?(letter)
+        @answer.index(letter) == letters.index(letter) ? 2 : 1
+      else
+        0
+      end
+    end
   end
 end
