5ba28e1e3578f3d68ba834cce5a00826c7cd78a5
Brad Pauly <bp@saturn.local>
2022-01-27 21:04:07 -0500

    Track hits, misses, and matches.

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -1,16 +1,46 @@
+require "set"
+
 class Wordle
+  class MaxGuessesExceeded < StandardError; end
+
   def initialize(answer)
     @answer = answer.to_s.upcase.split(//)
+    @guesses = []
+    @hits = Set.new
+    @matches = Set.new
+    @misses = Set.new
   end
 
   def guess(word)
+    @guesses << word
+    raise MaxGuessesExceeded, "Out of guesses" if @guesses.size > 6
+
     letters = word.to_s.upcase.split(//)
     letters.each_with_index.map do |letter, i|
       if @answer.include?(letter)
-        @answer[i] == letters[i] ? 2 : 1
+        if @answer[i] == letters[i]
+          @hits << letter
+          2
+        else
+          @matches << letter
+          1
+        end
       else
+        @misses << letter
         0
       end
     end
+  end
+
+  def hits
+    @hits.sort
+  end
+
+  def matches
+    @matches.sort
+  end
+
+  def misses
+    @misses.sort
   end
 end
--- spec/wordle_spec.rb
+++ spec/wordle_spec.rb
@@ -20,23 +20,112 @@ describe Wordle do
 
       assert_equal [2, 2, 2, 2, 2], wordle.guess("TEsts")
     end
+
+    it "returns an array of misses" do
+      wordle = Wordle.new("tests")
+  
+      assert_equal [0, 0, 0, 0, 0], wordle.guess("abcda")
+    end
+  
+    it "returns an array with matches" do
+      wordle = Wordle.new("tests")
+  
+      assert_equal [0, 1, 0, 0, 0], wordle.guess("atcda")
+    end
+  
+    it "handles duplicates correctly" do
+      wordle = Wordle.new("tests")
+  
+      assert_equal [2, 2, 2, 1, 1], wordle.guess("tesst")
+    end
+  
+    it "raises after 6 guesses" do
+      wordle = Wordle.new("tests")
+  
+      6.times { wordle.guess("tesst") }
+  
+      assert_raises(Wordle::MaxGuessesExceeded) { wordle.guess("tesst") }
+    end
   end
 
-  it "returns an array of misses" do
-    wordle = Wordle.new("tests")
+  describe "#hits" do
+    it "retunrs the letters that are hits" do
+      wordle = Wordle.new("tests")
 
-    assert_equal [0, 0, 0, 0, 0], wordle.guess("abcda")
+      wordle.guess("testa")
+
+      assert_equal wordle.hits, ["E", "S", "T"]
+    end
+
+    it "returns an empty set when there are no hits" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("treat")
+
+      assert_equal wordle.hits, []
+    end
+
+    it "accumulates hits for guesses" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("feeee")
+      wordle.guess("fleee")
+
+      assert_equal wordle.hits, ["F", "L"]
+    end
   end
 
-  it "returns an array with matches" do
-    wordle = Wordle.new("tests")
+  describe "#matches" do
+    it "retunrs the letters that are matches" do
+      wordle = Wordle.new("tests")
 
-    assert_equal [0, 1, 0, 0, 0], wordle.guess("atcda")
+      wordle.guess("plant")
+
+      assert_equal wordle.matches, ["T"]
+    end
+
+    it "returns an empty set when there are no matches" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("treat")
+
+      assert_equal wordle.matches, []
+    end
+
+    it "accumulates matches for guesses" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("ieeee")
+      wordle.guess("epeee")
+
+      assert_equal wordle.matches, ["I", "P"]
+    end
   end
 
-  it "handles duplicates correctly" do
-    wordle = Wordle.new("tests")
+  describe "#misses" do
+    it "retunrs the letters that are misses" do
+      wordle = Wordle.new("tests")
 
-    assert_equal [2, 2, 2, 1, 1], wordle.guess("tesst")
+      wordle.guess("testa")
+
+      assert_equal wordle.misses, ["A"]
+    end
+
+    it "returns an empty set when there are no misses" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("flips")
+
+      assert_equal wordle.misses, []
+    end
+
+    it "accumulates misses for guesses" do
+      wordle = Wordle.new("flips")
+
+      wordle.guess("flipa")
+      wordle.guess("flipe")
+
+      assert_equal wordle.misses, ["A", "E"]
+    end
   end
 end
